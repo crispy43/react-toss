352a4abd8d50b2d02bd6c7e10c5abced3622339c
jin <crispy4343@gmail.com>
2025-05-01 12:21:27 +0900

    송금 섹션 레이아웃

---
+++ app/routes/pages/home/components/transfer-service.tsx
@@ -0,0 +1,30 @@
+export default function TransferService() {
+  return (
+    <section className="h-[2311px] bg-amber-100">
+      <div className="container h-full bg-white py-[250px]">
+        <div className="bg-red-100">
+          <h2>송금</h2>
+          <p>
+            간편하고 안전하게
+            <br />
+            수수료는 평생 무료로,
+            <br />
+            이런 송금 써보셨나요?
+          </p>
+        </div>
+        <div className="flex justify-between bg-blue-100">
+          <div className="w-[510px] bg-blue-300">2-1</div>
+          <div className="w-[510px] bg-blue-500">2-2</div>
+        </div>
+        <div className="flex justify-between bg-green-100">
+          <div className="w-[510px] bg-green-300">3-1</div>
+          <div className="w-[510px] bg-green-500">3-2</div>
+        </div>
+        <div className="flex justify-between bg-orange-100">
+          <div className="w-[510px] bg-orange-300">4-1</div>
+          <div className="w-[510px] bg-orange-500">4-2</div>
+        </div>
+      </div>
+    </section>
+  );
+}
