b547cebdfeb441ddfb7b401f82cd2a0d53358538
jin <crispy4343@gmail.com>
2025-05-19 09:54:12 +0900

    관리자 홈 경로 변경 및 네비게이션 현재 경로 하이라이트 추가

--- app/routes/pages/admin/route.tsx
+++ app/routes/pages/admin/route.tsx
@@ -1,21 +1,5 @@
-import { type LoaderFunctionArgs, redirect } from 'react-router';
+import { redirect } from 'react-router';
 
-import { getAdminAuthSession } from '~/.server/services/session.service';
-import { BreadcrumbItem } from '~/components/ui/breadcrumb';
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const adminAuthSession = await getAdminAuthSession(request);
-  const adminAuth = adminAuthSession.getAdminAuth();
-  if (adminAuth) {
-    return {};
-  }
-  return redirect('/admin/login');
+export const loader = async () => {
+  return redirect('/admin/home');
 };
-
-export const handle = {
-  breadcrumb: () => <BreadcrumbItem>관리자 홈</BreadcrumbItem>,
-};
-
-export default function Admin() {
-  return <div>관리자 페이지</div>;
-}
